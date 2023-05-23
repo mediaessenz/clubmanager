a8fb26783bcd6d4c9ec1cd826a65b147372bb802
Alexander Grein <alexander.grein@gmail.com>
2023-05-23 12:07:19 +0200

    Remove jquery dependency from search location js

--- Classes/FormEngine/SearchLocationButton.php
+++ Classes/FormEngine/SearchLocationButton.php
@@ -3,41 +3,56 @@
 namespace Quicko\Clubmanager\FormEngine;
 
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Page\JavaScriptModuleInstruction;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Backend\Form\Element\InputTextElement;
 
 /**
- * 
+ *
  */
 class SearchLocationButton extends InputTextElement
 {
-  /**
-   * 
-   *
-   * @return array As defined in initializeResultArray() of AbstractNode
-   */
-  public function render()
-  {
-    $array = parent::render();
+    /**
+     *
+     *
+     * @return array As defined in initializeResultArray() of AbstractNode
+     * @throws \JsonException
+     */
+    public function render(): array
+    {
+        $resultArray = parent::render();
 
-    $mapping = $this->data['parameterArray']['fieldConf']['config']['mapping'];
-    $target = $this->data['parameterArray']['fieldConf']['config']['target'];
+        $mapping = $this->data['parameterArray']['fieldConf']['config']['mapping'];
+        $target = $this->data['parameterArray']['fieldConf']['config']['target'];
 
-    /** @var IconFactory $iconFactory */
-    $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
-    //    'data-mapping' => json_encode($mapping),
-    $array['html'] =  sprintf(
-      '<div class="clbmgr_search_location"><a href="#" class="clbmgr_search_location_button" data-mapping=\'%s\' data-target=\'%s\' data-name=\'%s\'  data-uid=\'%s\' data-fieldName=\'%s\' data-tableName=\'%s\' >%s</a></div>',
-      json_encode($mapping),
-      json_encode($target),
-      $this->data['parameterArray']['itemFormElName'],
-      $this->data['vanillaUid'],
-      $this->data['fieldName'],
-      $this->data['tableName'],
-      $iconFactory->getIcon('apps-toolbar-menu-search')
-    );
-    $array['requireJsModules'][] = 'TYPO3/CMS/Clubmanager/SearchLocation';
+        /** @var IconFactory $iconFactory */
+        $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+        $resultArray['html'] = sprintf(
+            '<div class="clbmgr_search_location"><a href="#" class="clbmgr_search_location_button" data-mapping=\'%s\' data-target=\'%s\' data-name=\'%s\'  data-uid=\'%s\' data-field-name=\'%s\' data-table-name=\'%s\' >%s</a></div>',
+            json_encode($mapping, JSON_THROW_ON_ERROR),
+            json_encode($target, JSON_THROW_ON_ERROR),
+            $this->data['parameterArray']['itemFormElName'],
+            $this->data['vanillaUid'],
+            $this->data['fieldName'],
+            $this->data['tableName'],
+            $iconFactory->getIcon('apps-toolbar-menu-search')
+        );
+//        To get this work, an es6 module js has to be created first
+//        $typo3Version = new Typo3Version();
+//        if ($typo3Version->getMajorVersion() > 11) {
+//            $resultArray['javaScriptModules'][] = JavaScriptModuleInstruction::create(
+//                '@quicko/clubmanager/SearchLocation.js'
+//            );
+//        } else {
+//            $resultArray['requireJsModules'][] = JavaScriptModuleInstruction::forRequireJS(
+//                'TYPO3/CMS/Clubmanager/SearchLocation'
+//            );
+//        }
+        $resultArray['requireJsModules'][] = JavaScriptModuleInstruction::forRequireJS(
+            'TYPO3/CMS/Clubmanager/SearchLocation'
+        );
 
-    return $array;
-  }
+        return $resultArray;
+    }
 }
--- Resources/Public/JavaScript/SearchLocation.js
+++ Resources/Public/JavaScript/SearchLocation.js
@@ -4,69 +4,69 @@
  * JavaScript to handle search requests
  * @exports TYPO3/CMS/GsaTemplate/CmSearchLocation
  */
-define(['jquery', 'TYPO3/CMS/Core/Ajax/AjaxRequest'], function ($, AjaxRequest) {
-  'use strict';
-  /**
-   * @exports TYPO3/CMS/Clubmanager/SearchLocation
-   */
-  var SearchLocation = {};
+define(['TYPO3/CMS/Core/Ajax/AjaxRequest'], function (AjaxRequest) {
+    'use strict';
+    /**
+     * @exports TYPO3/CMS/Clubmanager/SearchLocation
+     */
+    var SearchLocation = {};
 
-  /**
-   * @param {int} id
-   */
-  SearchLocation.run = function (uid, name, tableName, fieldName, mapping, target) {
+    /**
+     * @param {int} id
+     */
+    SearchLocation.run = function (uid, name, tableName, fieldName, mapping, target) {
 
-    var searchString = "";
-    for (var i = 0; i < mapping.length; i++) {
-      var dataId = "data[" + tableName + "][" + uid + "][" + mapping[i] + "]";
-      var input = $("[data-formengine-input-name='" + dataId + "']");
-      searchString += input.val() + " ";
-    }
-    searchString = searchString.trim();
-    if (!searchString) {
-      return;
-    }
-    new AjaxRequest('https://nominatim.openstreetmap.org/search')
-      .withQueryArguments({
-        format: "json",
-        limit: 1,
-        q: searchString
-      }
-      )
-      .get()
-      .then(async function (response) {
-        const result = await response.resolve();
-        if (result && result.length == 1) {
-          var data = result[0];
-          for (const [key, value] of Object.entries(target)) {
-            var dataId = "data[" + tableName + "][" + uid + "][" + key + "]";
-            var input = $("[data-formengine-input-name='" + dataId + "']");
-            input.val(data[value]);
-            TBE_EDITOR.fieldChanged(tableName, uid, key, dataId);
-          }
-          top.TYPO3.Notification.success('Fertig.');
-        } else {
-          top.TYPO3.Notification.warning('Keine Daten gefunden.');
+        let searchString = "";
+        mapping.forEach((element) => {
+                const dataId = `data[${tableName}][${uid}][${element}]`;
+                const input = document.querySelector(`[data-formengine-input-name="${dataId}"]`);
+                searchString += input.value + " ";
+            }
+        )
+        searchString = searchString.trim();
+        if (!searchString) {
+            return;
         }
-      });
+        new AjaxRequest('https://nominatim.openstreetmap.org/search')
+            .withQueryArguments({
+                    format: "json",
+                    limit: 1,
+                    q: searchString
+                }
+            )
+            .get()
+            .then(async function (response) {
+                const result = await response.resolve();
+                if (result && result.length == 1) {
+                    const data = result[0];
+                    for (const [key, value] of Object.entries(target)) {
+                        const input = document.querySelector(`[data-formengine-input-name="data[${tableName}][${uid}][${key}]"]`);
+                        input.value = data[value];
+                        input.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
+                    }
+                    top.TYPO3.Notification.success('Fertig.');
+                } else {
+                    top.TYPO3.Notification.warning('Keine Daten gefunden.');
+                }
+            });
 
-  };
+    };
 
-  /**
-   * initializes events using deferred bound to document
-   * so AJAX reloads are no problem
-   */
-  SearchLocation.initializeEvents = function () {
+    /**
+     * initializes events using deferred bound to document
+     * so AJAX reloads are no problem
+     */
+    SearchLocation.initializeEvents = function () {
 
-    $('.clbmgr_search_location_button').on('click', function (evt) {
-      evt.preventDefault();
+        document.querySelector('.clbmgr_search_location_button').addEventListener('click', (event) => {
+            event.preventDefault();
+            const target = event.currentTarget;
+            SearchLocation.run(target.dataset.uid, target.dataset.name, target.dataset.tableName, target.dataset.fieldName, JSON.parse(target.dataset.mapping), JSON.parse(target.dataset.target));
+        });
+    };
 
-      SearchLocation.run($(this).data("uid"), $(this).data("name"), $(this).data("tablename"), $(this).data("fieldname"), $(this).data("mapping"), $(this).data("target"));
-    });
-  };
+    SearchLocation.initializeEvents();
 
-  $(SearchLocation.initializeEvents);
-
-  return SearchLocation;
+    return SearchLocation;
 });
 
